a72b828fe5a6758e8ecaf1294d104b84ac1de402
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-09-11 15:06:15 +0700

    "add dark mode"

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,12 @@
 const Resume = () => {
   return (
     <div>
-      <main className="bg-gradient-to-b from-teal-500 to-white font-poppins">
+      <main className="bg-gradient-to-b from-teal-500 to-white font-poppins dark:from-gray-800 dark:to-black">
         <section className="min-h-screen flex items-center justify-center">
           <div className="text-center text-lg items-center justify-center">
-            <strong className="text-5xl">COMING SOON</strong>
+            <strong className="text-5xl text-black dark:text-white">
+              COMING SOON
+            </strong>
           </div>
         </section>
       </main>
