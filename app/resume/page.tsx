ae9ed3593951de8f54ee3f230ae7cc6ea00d3603
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-09-11 14:40:07 +0700

    changed text in resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ const Resume = () => {
       <main className="bg-gradient-to-b from-teal-500 to-white font-poppins">
         <section className="min-h-screen flex items-center justify-center">
           <div className="text-center text-lg items-center justify-center">
-            <strong>COMING SOON</strong>
+            <strong className="text-5xl">COMING SOON</strong>
           </div>
         </section>
       </main>
