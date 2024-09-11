2b94248a7bd631682f0bbe3f0ca948b7051c47e9
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-09-11 14:20:29 +0700

    Edited resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -2,7 +2,7 @@ const Resume = () => {
   return (
     <div>
       <main className="bg-gradient-to-b from-teal-500 to-white font-poppins">
-        <section className="min-h-screen">
+        <section className="min-h-screen flex items-center justify-center">
           <div className="text-center">
             <strong>COMING SOON</strong>
           </div>
