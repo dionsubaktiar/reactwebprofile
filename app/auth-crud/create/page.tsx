99f710b5c5da0a8689046bbfa4d22cffc6cabc04
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-12-27 01:39:14 +0700

    add a console.log

--- app/auth-crud/create/page.tsx
+++ app/auth-crud/create/page.tsx
@@ -55,7 +55,7 @@ const CreateArticlePage = () => {
         "https://personalproject.nusantaratranssentosa.co.id/sanctum/csrf-cookie",
         { withCredentials: true }
       );
-
+      console.log(userId);
       // Send API request to create an article
       await axios.post(
         "https://personalproject.nusantaratranssentosa.co.id/api/article",
