b1173b14b2ba1dda6ce0e7ef5a474ec591991af2
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2024-12-27 21:15:39 +0700

    deploy

--- app/auth-crud/edit/page.tsx
+++ app/auth-crud/edit/page.tsx
@@ -38,7 +38,12 @@ const EditArticlePage = () => {
     const fetchArticle = async () => {
       try {
         const response = await axios.get(
-          `https://personalproject.nusantaratranssentosa.co.id/api/article/${articleId}`
+          `https://personalproject.nusantaratranssentosa.co.id/api/article/${articleId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
         );
         setArticle(response.data.data);
       } catch (error) {
