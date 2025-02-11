db6e920cd69dfdee874e7aa60149214677dda282
dionsubaktiar <104469933+dionsubaktiar@users.noreply.github.com>
2025-02-12 01:02:01 +0700

    update

--- app/invoicing-service/page.tsx
+++ app/invoicing-service/page.tsx
@@ -2,6 +2,43 @@
 
 import Link from "next/link";
 import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Invoice {
+  id: number;
+  kilometer: number;
+  tanggal: string;
+  harga: number;
+  id_package: number;
+  status_invoice: string;
+  units: Unit;
+  packages: Package;
+}
+interface Customer {
+  id: number;
+  nama_perusahaan: string;
+  alamat: string;
+  pic: string;
+  kontak: string;
+  created_at: string;
+  updated_at: string;
+}
+
+interface Unit {
+  id: number;
+  nopol: string;
+  tipe: string;
+  driver: string;
+  tahun: string;
+  id_customer: number;
+  customers: Customer;
+}
+
+interface Package {
+  id: number;
+  kode_packages: string | null;
+}
 
 const MainPageInvoice = () => {
   const links = [
@@ -9,10 +46,27 @@ const MainPageInvoice = () => {
     { href: "/invoicing-service/customers", label: "Customers" },
   ];
 
+  const [apiResponse, setApiResponse] = useState<Invoice[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://personalproject.nusantaratranssentosa.co.id/api/invoice"
+        );
+        setApiResponse(response.data.data.data);
+      } catch (error) {
+        console.error("Error fetching units:", error);
+      }
+    };
+
+    fetchData();
+  });
+
   return (
     <div className="min-h-screen bg-customGreen-light text-brown dark:bg-gray-900 dark:text-honeyDew">
       <Navbar title="Invoice Service"></Navbar>
-      <div className="flex justify-center space-x-4">
+      <div className="flex justify-center gap-2 mb-4">
         {links.map(({ href, label }) => (
           <Link key={href} href={href}>
             <button className="bg-customGreen-default text-white px-4 py-2 rounded-lg shadow-md hover:bg-customGreen-dark transition duration-300">
@@ -20,6 +74,85 @@ const MainPageInvoice = () => {
             </button>
           </Link>
         ))}
+      </div>
+      <div className="overflow-x-auto bg-white dark:bg-gray-800 dark:text-honeyDew rounded-lg shadow-md mx-2">
+        <table className="min-w-full table-auto overflow-visible">
+          <thead>
+            <tr className="bg-customGreen-dark text-honeyDew">
+              <th className="px-4 py-2 text-left">Nomor Polisi</th>
+              <th className="px-4 py-2 text-left">Kilometer</th>
+              <th className="px-4 py-2 text-left">Jenis Service</th>
+              <th className="px-4 py-2 text-left">Customer</th>
+              <th className="px-4 py-2 text-left">Tanggal</th>
+              <th className="px-4 py-2 text-left">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apiResponse.map((invoice) => (
+              <tr key={invoice.id} className="border-t">
+                <td className="px-4 py-2 text-nowrap">{invoice.units.nopol}</td>
+                <td className="px-4 py-2 text-nowrap">
+                  {invoice.kilometer} Km
+                </td>
+                {invoice.packages ? (
+                  <td className="px-4 py-2 text-nowrap">
+                    {invoice.packages?.kode_packages}
+                  </td>
+                ) : (
+                  <td className="px-4 py-2 text-nowrap">ADHOC</td>
+                )}
+                <td className="px-4 py-2">
+                  {invoice.units.customers.nama_perusahaan}
+                </td>
+                <td className="px-4 py-2 text-nowrap">
+                  {new Date(invoice.tanggal).toLocaleDateString("id-ID", {
+                    day: "numeric",
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </td>
+                {/* <td className="px-4 py-2 relative">
+                  <div className="relative inline-block text-left">
+                    <button
+                      onClick={() =>
+                        setOpenDropdown(
+                          openDropdown === unit.id ? null : unit.id
+                        )
+                      }
+                      className="bg-gray-200 text-eggplant px-3 py-1 rounded-md hover:bg-gray-300 transition"
+                    >
+                      Actions
+                    </button>
+                    {openDropdown === unit.id && (
+                      <div className="absolute right-0 top-full w-32 bg-white border border-gray-200 rounded-md shadow-lg z-50">
+                        <Link
+                          href={`/invoicing-service/units/edit?id=${unit.id}`}
+                        >
+                          <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
+                            Edit
+                          </button>
+                        </Link>
+                        <Link
+                          href={`/invoicing-service/units/view?id=${unit.id}`}
+                        >
+                          <button className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100">
+                            View
+                          </button>
+                        </Link>
+                        <button
+                          onClick={() => handleDelete(unit.id)}
+                          className="block w-full text-left px-4 py-2 text-red-600 hover:bg-gray-100"
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                </td> */}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
